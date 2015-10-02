fa079c5435cd85f9642e4f90e7469146a4ad37e1
Brian LaMattina <brianlamattina@gmail.com>
2015-10-01 20:31:20 -0400

    Integrate DestinationParser into cli

--- bin/issue-comment-cli.js
+++ bin/issue-comment-cli.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 var argv = require('minimist')(process.argv.slice(1)),
     help = require('help')(__dirname + '/../doc/issue-comment.txt'),
-    debug = require('debug')('github-hotline:issue-comment-cli');
+    debug = require('debug')('github-hotline:issue-comment-cli'),
+    chalk = require('chalk'),
+    DestinationParser = require('../lib/destination-parser');
 
 var destination = argv._[1],
     comment = argv._.slice(2).join(' ');
+
+var reportError = function(error) {
+  debug('Uncaught exception. Exiting!');
+  debug(error);
+  console.error(chalk.red('\nERROR: ' + error.message + '\n'));
+  help(1);
+};
 
 if (!(destination && comment)) {
   debug('Invalid arguments: ');
@@ -13,5 +22,16 @@ if (!(destination && comment)) {
 } else {
   debug('Destination: ' + destination);
   debug('Comment: ' + comment);
-  console.log(destination + comment);
+  try {
+    var destinationParser = new DestinationParser(destination);
+    console.log({
+      user: destinationParser.getUser(),
+      repo: destinationParser.getRepo(),
+      number: destinationParser.getNumber(),
+      comment: comment
+    });
+
+  } catch (error) {
+    reportError(error);
+  }
 }
