8ea623e357e67ecfb1af5274295283e1a2e33314
Brian LaMattina <brianlamattina@gmail.com>
2015-10-05 21:37:29 -0400

    Add --user, --repo, and --number flags

--- bin/issue-comment-cli.js
+++ bin/issue-comment-cli.js
@@ -1,37 +1,57 @@
 #!/usr/bin/env node
-var argv = require('minimist')(process.argv.slice(1)),
+var _ = require('lodash'),
+    argv = require('minimist')(process.argv.slice(2)),
     help = require('help')(__dirname + '/../doc/issue-comment.txt'),
     debug = require('debug')('github-hotline:issue-comment-cli'),
     chalk = require('chalk'),
     DestinationParser = require('../lib/destination-parser');
 
-var destination = argv._[1],
-    comment = argv._.slice(2).join(' ');
-
-var reportError = function(error) {
+function reportError(error) {
   debug('Uncaught exception. Exiting!');
   debug(error);
   console.error(chalk.red('\nERROR: ' + error.message + '\n'));
   help(1);
 };
 
-if (!(destination && comment)) {
-  debug('Invalid arguments: ');
-  debug(argv);
-  help(0);
-} else {
-  debug('Destination: ' + destination);
-  debug('Comment: ' + comment);
-  try {
-    var destinationParser = new DestinationParser(destination);
-    console.log({
-      user: destinationParser.getUser(),
-      repo: destinationParser.getRepo(),
-      number: destinationParser.getNumber(),
-      comment: comment
-    });
+function parseArgs(argv) {
+  if (_(argv).has('user') && _(argv).has('repo') && _(argv).has('number')) {
+    var user = argv['user'],
+        repo = argv['repo'],
+        number = argv['number'];
+        comment = argv._.slice(0).join(' ');
 
-  } catch (error) {
-    reportError(error);
+  } else {
+    var destination = argv._[0],
+        comment = argv._.slice(1).join(' ');
+
+    var destinationParser = new DestinationParser(destination),
+        user = destinationParser.getUser(),
+        repo = destinationParser.getRepo(),
+        number = destinationParser.getNumber;
   }
+  return {
+    user: user,
+    repo: repo,
+    number: number,
+    comment: comment
+  }
+}
+
+try {
+  var params = parseArgs(argv);
+  if (params.user && params.repo && params.number && params.comment) {
+    debug('Issue comment with: ', params);
+    console.log({
+      user: params.user,
+      repo: params.repo,
+      number: params.number,
+      comment: params.comment
+    });
+  } else {
+    debug('Invalid arguments: ');
+    debug(argv);
+    help(0);
+  }
+} catch (error) {
+  reportError(error);
 }
